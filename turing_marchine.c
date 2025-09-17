b4fab5b7b6b0efc982cc0223587dc049d4f48eb5
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-09-17 10:02:24 -0300

    First commit

---
+++ turing_marchine.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+struct Node
+{
+    struct Node *next;
+    int value;
+};
+
+struct HashTable
+{
+    struct Node **vector;
+}
+
+/*
+struct State
+{
+    
+}; */
+
+/*
+struct Automaton
+{
+    int 
+};
+*/
+
+int main(int argc, char **argv)
+{
+    
+}
